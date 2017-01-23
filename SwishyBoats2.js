9f902960a1238d4836b458465312e766a122a309
annakraemer <annakraemer@dpengineering.org>
2017-01-23 10:51:42 -0800

    🥞

--- SwishyBoats2.js
+++ SwishyBoats2.js
@@ -28,17 +28,18 @@ function SwishyBoats2(){
 
   
  //slide to control speed - still am working on finding the correct dimensions for each of the sliders
-  this.fixedPositionSlider =  new Slider(
+  this.slider1 =  new Slider(
                            windowWidth*0.675 - 100, // x position .18
                            windowHeight*0.3 - 45, // y position
                            windowWidth*0.35, // size of slider
                            0, // min value of slider
                            100, // max value of slider
                            100, // default value of slider
-                           this.fixedChangePosition.bind(this)); // action to call on slider change
-  this.fixedPositionSlider.sliderImage(rightBoat); // "sliderImage" sets the image of the knob of the slider object
-  this.addActor(this.fixedPositionSlider); // adds slider to scene, need to figure out how to input the images of the boats into the scene 
-  //this.fixedPositionSlider.loadImg(leftBoat);  
+                           this.position1.bind(this)); // action to call on slider change
+  this.slider1.sliderImage(rightBoat); // "sliderImage" sets the image of the knob of the slider object
+  this.addActor(this.slider1); // adds slider to scene, need to figure out how to input the images of the boats into the scene 
+  //this.fixedPositionSlider.loadImg(leftBoat); 
+  
   //slider label for Boat 1 position
   this.fixedSlideLabel = new Label(
                        windowWidth*.675 - 100, // x position .25
@@ -48,71 +49,89 @@ function SwishyBoats2(){
   this.addActor(this.fixedSlideLabel); // adds text to scene
   
    //slide to control speed
-  this.fixedPositionSlider =  new Slider(
+  this.slider2 =  new Slider(
                            windowWidth*0.475 - 100, // x position .26
                            windowHeight*0.45 - 52, // y position .5
                            windowWidth*0.35, // size of slider .5
-                           0, // min value of slider
-                           100, // max value of slider
-                           100, // default value of slider
-                           this.fixedChangePosition.bind(this)); // action to call on slider change
-  this.fixedPositionSlider.sliderImage(leftBoat); // "sliderImage" sets the image of the knob of the slider object
-  this.addActor(this.fixedPositionSlider); // adds slider to scene
+                           0, 
+                           100, 
+                           100, 
+                           this.position2.bind(this)); 
+  this.slider2.sliderImage(leftBoat); 
+  this.addActor(this.slider2); 
   
   //slider label for Boat 2 position
   this.fixedSlideLabel = new Label(
-                       windowWidth*0.475 - 100, // x position
-                       windowHeight*0.45 - 70, // y position
-                       "Boat 2 Position", // text
-                       {size: windowWidth*0.035, leading: windowHeight*0.09}); // text attributes
-  this.addActor(this.fixedSlideLabel); // adds text to scene
+                       windowWidth*0.475 - 100, 
+                       windowHeight*0.45 - 70, 
+                       "Boat 2 Position", 
+                       {size: windowWidth*0.035, leading: windowHeight*0.09}); 
+  this.addActor(this.fixedSlideLabel); 
   
    //slide to control speed
-  this.fixedPositionSlider =  new Slider(
-                           windowWidth*0.675 - 100, // x position
-                           windowHeight*0.6 - 2, // y position
-                           windowWidth*0.35, // size of slider
-                           0, // min value of slider
-                           100, // max value of slider
-                           0, // default value of slider
-                           this.fixedChangePosition.bind(this)); // action to call on slider change
-  this.fixedPositionSlider.sliderImage(rightBoat); // "sliderImage" sets the image of the knob of the slider object
-  this.addActor(this.fixedPositionSlider); // adds slider to scene
+  this.slider3 =  new Slider(
+                           windowWidth*0.675 - 100, 
+                           windowHeight*0.6 - 2, 
+                           windowWidth*0.35, 
+                           0, 
+                           100, 
+                           0, 
+                           this.position3.bind(this)); 
+  this.slider3.sliderImage(rightBoat); 
+  this.addActor(this.slider3); 
   
   //slider label for Boat 3 position 
   this.fixedSlideLabel = new Label(
-                       windowWidth*0.675 - 100, // x position
-                       windowHeight*0.6 - 10, // y position
-                       "Boat 3 Position", // text
-                       {size: windowWidth*0.035, leading: windowHeight*0.09}); // text attributes
-  this.addActor(this.fixedSlideLabel); // adds text to scene
+                       windowWidth*0.675 - 100, 
+                       windowHeight*0.6 - 10, 
+                       "Boat 3 Position", 
+                       {size: windowWidth*0.035, leading: windowHeight*0.09}); 
+  this.addActor(this.fixedSlideLabel); 
   
    //slide to control speed
-  this.fixedPositionSlider =  new Slider(
-                           windowWidth*0.475 - 100, // x position
-                           windowHeight*0.85 - 40, // y position
-                           windowWidth*0.35, // size of slider
-                           0, // min value of slider
-                           100, // max value of slider
-                           100, // default value of slider
-                           this.fixedChangePosition.bind(this)); // action to call on slider change
-  this.fixedPositionSlider.sliderImage(leftBoat); // "sliderImage" sets the image of the knob of the slider object
-  this.addActor(this.fixedPositionSlider); // adds slide to scene
+  this.slider4 =  new Slider(
+                           windowWidth*0.475 - 100, 
+                           windowHeight*0.85 - 40, 
+                           windowWidth*0.35, 
+                           0, 
+                           100, 
+                           100, 
+                           this.position4.bind(this)); 
+  this.slider4.sliderImage(leftBoat); 
+  this.addActor(this.slider4); 
  
    this.fixedSlideLabel = new Label(
-                       windowWidth*0.475 - 100, // x position
-                       windowHeight*0.85 - 50, // y position
-                       "Boat 4 Position", // text
-                       {size: windowWidth*0.035, leading: windowHeight*0.09}); // text attributes
-  this.addActor(this.fixedSlideLabel); // adds text to scene
+                       windowWidth*0.475 - 100, 
+                       windowHeight*0.85 - 50, 
+                       "Boat 4 Position", 
+                       {size: windowWidth*0.035, leading: windowHeight*0.09}); 
+  this.addActor(this.fixedSlideLabel); 
 }
 
 _inherits(SwishyBoats2, Scene);
 
 
-//update slider position
-SwishyBoats2.prototype.fixedChangePosition = function(slidePosition) {
+//update slider position on boat 1
+SwishyBoats2.prototype.position1 = function(slidePosition) {
   console.log("Current value of slider is " + slidePosition);
   manager.change(SWISHYBOATS.master.values.boat1Pos, slidePosition);
+}
+
+//update slider position on boat 2
+SwishyBoats2.prototype.position2 = function(slidePosition) {
+  console.log("Current value of slider is " + slidePosition);
+  manager.change(SWISHYBOATS.master.values.boat2Pos, slidePosition);
+}
+
+//update slider position on boat 3
+SwishyBoats2.prototype.position3 = function(slidePosition) {
+  console.log("Current value of slider is " + slidePosition);
+  manager.change(SWISHYBOATS.master.values.boat3Pos, slidePosition);
+}
+
+//update slider position on boat 4
+SwishyBoats2.prototype.position4 = function(slidePosition) {
+  console.log("Current value of slider is " + slidePosition);
+  manager.change(SWISHYBOATS.master.values.boat4Pos, slidePosition);
 }
   
