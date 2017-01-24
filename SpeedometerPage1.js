f134fd5522ccbae7d35b15394f068dcb5ff676cd
annakraemer <annakraemer@dpengineering.org>
2017-01-24 09:00:37 -0800

    changed FishyTank

--- SpeedometerPage1.js
+++ SpeedometerPage1.js
@@ -81,9 +81,57 @@ SpeedometerPage1.prototype.finishedAction = function() {
 }
 */
 
-//FishyTank - edited by Chloe
+//FishyTank - edited by Chloe, edited Sophia
 
 "use strict";
+
+function FishyTank(){
+
+ /////////////////////////////// BASIC SETUP ///////////////////////////////
+	
+  Scene.call(this);
+  
+  attrs = {size:25, leading:25};
+  
+  //Setting maximum variables
+  this.maxDistance = 200;
+  
+  this.bgBorder = new BackgroundBorder();
+  this.addActor(this.bgBorder);
+  
+  this.homeButton = new HomeButton(homeAction);
+  this.addActor(this.homeButton);
+  
+  this.title = new Label(windowWidth/2, windowHeight*0.16, "Fishy Tank", {size:70, leading:50});
+  this.addActor(this.title);
+}
+
+_inherits(SpeedometerPage1, Scene);
+
+  
+  
+  //this.nextButton = new NextButton(this.runCommand.bind(this));
+  //this.addActor(this.nextButton);
+
+  /////////////////////////////// BUTTON AND DIAL /////////////////////////////////
+  
+  //demo button
+  //PUT IN PICTURE FOR BUTTON
+  this.demo1 = new TextButton(
+                           windowWidth*0.475 - 100, // x position
+									         windowHeight*0.8 - 50, // y position
+									         300, // width of button
+									         100, // height of button
+									         BLUE, // color of button
+									         "Demo", // text on button
+									         attrs, // text attributes
+									         this.demo1Action.bind(this), // action to call
+									         'rect'); // shape
+  this.addActor(this.demo1); // Adds button "actor" to the scene
+  
+  
+  
+  
 
 function SpeedometerPage1()
 {
@@ -122,7 +170,9 @@ function SpeedometerPage1()
   
 }
 
-_inherits(SpeedometerPage1, Scene);
+//_inherits(SpeedometerPage1, Scene);
+
+///////////////////////////////// BUTTON SUB FUNCTIONS //////////////////////////////////
 
 SpeedometerPage1.prototype.setup = function(){
    manager.changeState(STATE_FISHYTANK);
