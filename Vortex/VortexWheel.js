eb3de5ca3574ca2ef37901d5515fd76f67a4adca
annakraemer <annakraemer@dpengineering.org>
2017-02-14 10:16:43 -0800

    Add files via upload

--- Vortex/VortexWheel.js
+++ Vortex/VortexWheel.js
@@ -8,54 +8,46 @@ function VortexWheel(){
 	
   Scene.call(this);
   
-  
   attrs = {size:25, leading:25};
   
   //Setting maximum variables
+  
   this.maxDistance = 200;
   
   this.bgBorder = new BackgroundBorder();
   this.addActor(this.bgBorder);
   
+  //create home button
+  
   this.homeButton = new HomeButton(homeAction);
   this.addActor(this.homeButton);
+  
+  //title the scene
   
   this.title = new Label(windowWidth/2, windowHeight*0.16, "Magnetic Vortex", {size:70, leading:50});
   this.addActor(this.title);
   
-  /*
- 
+  //create wheel
+  //function Wheel(x, y, width, height, onChange, deadZoneSize, scaler, minAngle, maxAngle)
   var wheelSize = 350;
-  this.location = new Speedometer(windowWidth*0.20 - wheelSize/2, windowHeight/2 - wheelSize/2,
-     wheelSize, wheelSize, this.locationAction.bind(this), 0, 1, 135, 45);
-  this.locationLabel = new Label(windowWidth*0.20, windowHeight/2 + 150, "Degrees: 0 ", attrs);
-  this.addActor(this.location);
-  this.addActor(this.locationLabel); //creates the speedometer object in this scene, as well as a label for the value of it
-
-	this.location.displayAngle = 270;
-  this.location.onChange(270);
-  */
-  
-  
-  //function Speedometer(x, y, width, height, onChange, deadZoneSize, scaler, minAngle, maxAngle) {-
-  var wheelSize = 350;
-  this.wheel = new Speedometer(windowWidth*0.5 - wheelSize/2, windowHeight*0.75 - wheelSize/2, wheelSize, wheelSize, this.MoveToAngleAction.bind(this), 0, 1, 135, 45);
+  this.wheel = new ImageWheel(windowWidth*0.5 - wheelSize/2, windowHeight*0.5 - wheelSize/2, wheelSize, wheelSize, this.MoveToAngleAction.bind(this), 0,2,0,10000000000000000000000000000000); //max angle is so large so as to allow the dial to spin all the way around many times
   this.addActor(this.wheel);
-
+  
 }
 
 _inherits(VortexWheel, Scene);
 
+ ///////////////////////////////MAGNETIC VORTEX FUNCTIONS///////////////////////////////
 
 VortexWheel.prototype.setup = function(){
   console.log("Changed state to MAGNETICVORTEX");
   manager.changeState(STATE_MAGNETICVORTEX);
 }
 
-
+//move the vortex to the value of the dial in UI
 VortexWheel.prototype.MoveToAngleAction = function(dialValue){
   console.log(dialValue);
-  manager.change(MAGNETICVORTEX.master.values.dialPosition,dialValue);
+ // manager.change(MAGNETICVORTEX.master.values.dialPosition,dialValue);
   console.log("After: " + MAGNETICVORTEX.master.values.dialPosition)
 }
 
