b075d849ceea7c2b7cb7247d0c58c84d10d4fcbe
annakraemer <annakraemer@dpengineering.org>
2017-02-28 08:22:50 -0800

    Add files via upload

--- SwishyBoats/SwishyBoatsMenuScene.js
+++ SwishyBoats/SwishyBoatsMenuScene.js
@@ -12,6 +12,7 @@ function SwishyBoatsMenuScene() {
   attrs = {size:50, leading:25};
   
   //Setting maximum variables
+  
   this.maxDistance = 200;
   
   this.bgBorder = new BackgroundBorder();
@@ -20,12 +21,13 @@ function SwishyBoatsMenuScene() {
   this.homeButton = new HomeButton(homeAction);
   this.addActor(this.homeButton);
   
-  this.title = new Label(windowWidth/2, windowHeight*0.16, "Swishy Boats", {size:70, leading:50});
+  this.title = new Label(windowWidth/2, windowHeight*0.16, "Swishy Boats Menu", {size:70, leading:50});
   this.addActor(this.title);
+  
+ /////////////////////////////// BUTTONS /////////////////////////////////
 
-    /////////////////////////////// BUTTONS /////////////////////////////////
-
-  // creating new buttons for demo and manual scene for user to choose between which scene to go to
+ // creating new buttons for demo and manual scene for user to choose between which scene to go to
+  
   this.demo = new TextButton( 
                            windowWidth*0.2 - 100, // x position
 									         windowHeight*0.5 - 50, // y position
@@ -38,36 +40,39 @@ function SwishyBoatsMenuScene() {
 									         'rect'); // shape
   this.addActor(this.demo); // Adds button "actor" to the scene
   
-  
   this.manual = new TextButton(
                            windowWidth*0.6 - 100, // x position
 									         windowHeight*0.5 - 50, // y position
 									         500, // width of button
 									         150, // height of button
 									         BLUE, // color of button
-									         "Manual Play", // text on button
+									         "Play Game", // text on button
 									         attrs, // text attributes
 									         this.manualAction.bind(this), // action to call
 									         'rect'); // shape
   this.addActor(this.manual); // Adds button "actor" to the scene
   
-
-  
 }
 
 _inherits(SwishyBoatsMenuScene, Scene);
 
-  ///////////////////////////////// BUTTON SUB FUNCTIONS //////////////////////////////////
+ ///////////////////////////////// BUTTON SUB FUNCTIONS //////////////////////////////////
 
-SwishyBoatsMenuScene.prototype.setup = function(){ //telling the User INterface to change the states
+ //change state into SWISHYBOATS//
+ 
+SwishyBoatsMenuScene.prototype.setup = function(){ 
   console.log("Changed state to SWISHYBOATS");
   manager.changeState(STATE_SWISHYBOATS);
 }
 
+//transition to demo scene//
+
 SwishyBoatsMenuScene.prototype.demoAction = function() {
   console.log ("demo transition");
-  stage.transitionTo('SwishyBoats');
+  //stage.transitionTo('SwishyBoats'); // this should be a demo function
 }
+
+//transition to the manual play scene//
 
 SwishyBoatsMenuScene.prototype.manualAction = function() {
   console.log("manual transition");
